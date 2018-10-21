04255793f2cccf63466350461bba57509c5d591c
OlegKolov <43613244+OlegKolov@users.noreply.github.com>
2018-10-21 09:11:53 +0300

    Contains "main" with execution of classes(Engine, Driver, Car).

---
+++ src/homeProject8/Ex3/com/company/ExCar.java
@@ -0,0 +1,19 @@
+package homeProject8.Ex3.com.company;
+
+import homeProject8.Ex3.com.company.details.Engine;
+import homeProject8.Ex3.com.company.professions.Driver;
+import homeProject8.Ex3.com.company.vehicles.Car;
+
+public class ExCar {
+    public static void main(String[] args) {
+        Driver firstDriver = new Driver("Ivanov Ivan Ivanovich", 22, 2.5);
+        Engine firstEngine = new Engine(200, "Volkswagen");
+        Car firstCar = new Car("Volkswagen", "S", 2500, firstDriver, firstEngine);
+
+        firstCar.start();
+        firstCar.stop();
+        firstCar.turnRight();
+        firstCar.turnLeft();
+        firstCar.printInfo();
+    }
+}
