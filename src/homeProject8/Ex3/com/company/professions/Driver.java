0027570a92dbb7d54b6c32c350039e065b51bbbb
OlegKolov <43613244+OlegKolov@users.noreply.github.com>
2018-10-21 09:07:26 +0300

    Driver class extends Person(homeProject7). Variable experience.

---
+++ src/homeProject8/Ex3/com/company/professions/Driver.java
@@ -0,0 +1,24 @@
+package homeProject8.Ex3.com.company.professions;
+
+import homeProject7.Person;
+
+public class Driver extends Person {
+    private double experience;
+
+    public Driver(String fio, int age, double experience) {
+        super(fio, age);
+        this.experience = experience;
+    }
+
+    public Driver() {
+    }
+
+    public double getExperience() {
+        return experience;
+    }
+
+
+    public void setExperience(double experience) {
+        this.experience = experience;
+    }
+}
