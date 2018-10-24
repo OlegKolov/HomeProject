94d73da3f29f47a43dcfc045814d72b6595e41f8
OlegKolov <43613244+OlegKolov@users.noreply.github.com>
2018-10-24 18:17:28 +0300

    Contains execution of class Fruit and its extenders.

---
+++ src/homeProject9/Ex4/ExFruit.java
@@ -0,0 +1,25 @@
+package homeProject9.Ex4;
+
+public class ExFruit {
+    public static void main(String[] args) {
+        Fruit apple1 = new Apple(3.5);
+        Fruit apple2 = new Apple(3.7);
+        Fruit pear1 = new Pear(2.3);
+        Fruit pear2 = new Pear(3.5);
+        Fruit pear3 = new Pear(1.5);
+        Fruit apricot1 = new Apricot(4.8);
+        Fruit apricot2 = new Apricot(2.1);
+        double result = 0;
+
+        Fruit[] fruits = {apple1, apple2, pear1, pear2, pear3, apricot1, apricot2};
+
+        for (Fruit f : fruits) {
+            result += f.cost();
+        }
+        System.out.println(result);
+
+        System.out.println(Apple.getSum());
+        System.out.println(Pear.getSum());
+        System.out.println(Apricot.getSum());
+    }
+}
