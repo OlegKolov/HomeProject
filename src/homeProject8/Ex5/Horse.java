31420c9aecac472dc768e96b31391f8ff7da2327
OlegKolov <43613244+OlegKolov@users.noreply.github.com>
2018-10-24 16:51:19 +0300

    Class Horse. Contains variables horseshoes, usage. Methods: makeNoise, eat, sleep.

---
+++ src/homeProject8/Ex5/Horse.java
@@ -0,0 +1,51 @@
+package homeProject8.Ex5;
+
+public class Horse extends Animal {
+    private boolean horseshoes;
+    private String usage;
+
+    public Horse(String food, String location, boolean horseshoes, String usage) {
+        super(food, location);
+        this.horseshoes = horseshoes;
+        this.usage = usage;
+    }
+
+    public Horse(boolean horseshoes, String usage) {
+        this.horseshoes = horseshoes;
+        this.usage = usage;
+    }
+
+    public Horse() {
+    }
+
+    public boolean isHorseshoes() {
+        return horseshoes;
+    }
+
+    public void setHorseshoes(boolean horseshoes) {
+        this.horseshoes = horseshoes;
+    }
+
+    public String getUsage() {
+        return usage;
+    }
+
+    public void setUsage(String usage) {
+        this.usage = usage;
+    }
+
+    @Override
+    public void makeNoise(){
+        System.out.println("Neeeeeigh");
+    }
+
+    @Override
+    public void eat(){
+        System.out.println("Horse is eating");
+    }
+
+    @Override
+    public void sleep(){
+        System.out.println("Horse is sleeping");
+    }
+}
