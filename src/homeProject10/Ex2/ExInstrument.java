3958086b26798e7be0e681cf0878cdc2389db44a
OlegKolov <43613244+OlegKolov@users.noreply.github.com>
2018-10-28 09:25:58 +0200

    The implementation of the project with musical instruments.

--- src/homeProject10/Ex2/ExInstrument.java
+++ src/homeProject10/Ex2/ExInstrument.java
@@ -11,7 +11,7 @@ public class ExInstrument {
 
         Instrument[] instruments = {instrument1, instrument2, instrument3, instrument4, instrument5, instrument6};
 
-        for (Instrument i : instruments){
+        for (Instrument i : instruments) {
             i.play();
         }
     }
