54bbdf93702e4b8f851eb14c139efeb296fe1379
Oleg <olehole@ukr.net>
2018-11-07 18:46:08 +0300

    Work with some String methods.

---
+++ src/homeProject11/ex1/WorkWithStrings.java
@@ -0,0 +1,19 @@
+package homeProject11.ex1;
+
+public class WorkWithStrings {
+    public static void main(String[] args) {
+        String line = "I like Java!!!";
+        workWithString(line);
+    }
+
+    public static void workWithString(String line){
+        System.out.println("Last character is - " + line.charAt(line.length()-1));
+        System.out.println("Ends with '!!!' ? " + line.endsWith("!!!"));
+        System.out.println("Starts with 'I like' ? " + line.startsWith("I like"));
+        System.out.println("Start index of string 'Java' - " + line.indexOf("Java"));
+        System.out.println("String after chang 'a' -> 'o' " + line.replace('a','o'));
+        System.out.println("String after upper case " + line.toUpperCase());
+        System.out.println("String after lower case " + line.toLowerCase());
+        System.out.println("String after substring " + line.substring(line.indexOf("Java"),line.indexOf("Java") + "Java".length()));
+    }
+}
