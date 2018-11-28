42eaae78b373a747cd5fc496b45a9fb741dac0ed
OlegKolov <olehole@ukr.net>
2018-11-28 18:49:30 +0300

    Main

--- src/homeProject16/ex2/Main.java
+++ src/homeProject16/ex2/Main.java
@@ -1,12 +1,27 @@
 package homeProject16.ex2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        double[] d = {1, 2, 3};
         Student student1 = new Student("Artem", "A-1", 3, 1, 2, 3);
         Student student2 = new Student("Artur", "B-1", 4, 4, 5, 3);
         Student student3 = new Student("Artemon", "A-1", 3, 5, 3, 3);
 
-
+        List<Student> setStudent = new ArrayList<>();
+        setStudent.add(student1);
+        setStudent.add(student2);
+        setStudent.add(student3);
+       /* for (Student s; setStudent) {
+           double result = avar.
+                    if (result >= 3){
+                        System.out.println("");
+                    }
+        }
     }
-}
+
+    public static avar(Student s){
+
+    }*/
+}}
