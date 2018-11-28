4932411023b89998d8c76cb9987df12f5769b48c
OlegKolov <olehole@ukr.net>
2018-11-28 18:29:42 +0300

    Main

---
+++ src/homeProject16/ex2/Main.java
@@ -0,0 +1,12 @@
+package homeProject16.ex2;
+
+public class Main {
+    public static void main(String[] args) {
+        double[] d = {1, 2, 3};
+        Student student1 = new Student("Artem", "A-1", 3, 1, 2, 3);
+        Student student2 = new Student("Artur", "B-1", 4, 4, 5, 3);
+        Student student3 = new Student("Artemon", "A-1", 3, 5, 3, 3);
+
+
+    }
+}
